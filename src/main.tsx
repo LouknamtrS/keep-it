38c1e96903beeff34ac183f0304c458ebe89f6e7
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2026-04-02 15:17:51 +0700

    setup app and main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./App.css";
+import "./index.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+ import RegisterPage from "./pages/Register";
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </>
+  );
+}
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
---
+++ src/pages/Register.tsx
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <h1 className="text-2xl font-bold text-center">Create Account</h1>
+      </div>
+    </main>
+  );
+}
