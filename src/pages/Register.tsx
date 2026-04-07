8e7f697b3eb95b16941f651545b494b94a2d47b2
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2026-04-07 18:12:55 +0700

    fix tailwind version and add config file

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,8 @@
 export default function RegisterPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <h1 className="text-2xl font-bold text-center">Create Account</h1>
+    <main className="flex items-center justify-center md:h-screen bg-bg-peach w-full h-full">
+      <div className="relative mx-auto flex w-full flex-col space-y-2.5 p-4 md:-mt-32">
+        <h1 className="text-2xl font-bold text-center text-red-100">Create Account</h1>
       </div>
     </main>
   );
